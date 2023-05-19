7d23b4d86cd5278b2fc78bdacf646de03758cb14
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2023-05-19 19:58:01 +0330

    #5 Has been Added Automapper and MediatR

---
+++ HR.LeaveManagement.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace HR.LeaveManagement.Application;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services) =>
+        services.AddAutoMapper(Assembly.GetExecutingAssembly())
+        .AddMediatR(config => config.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+}
