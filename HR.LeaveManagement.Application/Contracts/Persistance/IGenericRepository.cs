892cb53909b40a410b6d81b30f655980e03719f9
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2023-05-19 19:33:56 +0330

    #4 Has been Created Generic Repository Interface

---
+++ HR.LeaveManagement.Application/Contracts/Persistance/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using HR.LeaveManagement.Domain.Common;
+using System.Linq.Expressions;
+
+namespace HR.LeaveManagement.Application.Contracts.Persistance;
+
+public interface IGenericRepository<T>
+    where T : BaseEntity
+{
+    Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+        Expression<Func<T, object>> include = null,
+        Expression<Func<T, object>> order = null,
+        bool ascending = true,
+        int page = 1,
+        int pageSize = 20,
+        CancellationToken cancellation = default);
+    Task<T> GetByIdAsync(int id, CancellationToken cancellation = default);
+    Task<T> CreateAsync(T entity, CancellationToken cancellation = default);
+    Task<T> UpdateAsync(T entity, CancellationToken cancellation = default);
+    Task DeleteAsync(T entity, CancellationToken cancellation = default);
+}
