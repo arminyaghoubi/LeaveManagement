1c3bf444e55fd10c7c0bec08ccf75fb0d153d1b6
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2023-05-20 22:36:59 +0330

    #7 Has been Created LeaveTypes Query and Query Handler

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAll/GetLeaveTypesQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAll;
+
+public record GetLeaveTypesQuery(int page, int pageSize) : IRequest<IEnumerable<LeaveTypeDto>>;
---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAll/GetLeaveTypesQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistance;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAll;
+
+public class GetLeaveTypesQueryHandler : IRequestHandler<GetLeaveTypesQuery, IEnumerable<LeaveTypeDto>>
+{
+    private readonly IMapper _mapper;
+    private readonly IGenericRepository<Domain.LeaveType> _repository;
+
+    public GetLeaveTypesQueryHandler(IMapper mapper,
+        IGenericRepository<Domain.LeaveType> repository)
+    {
+        _mapper = mapper;
+        _repository = repository;
+    }
+
+    public async Task<IEnumerable<LeaveTypeDto>> Handle(GetLeaveTypesQuery request, CancellationToken cancellationToken)
+    {
+        var leaveTypes = await _repository.GetAsync(page: request.page, pageSize: request.pageSize);
+
+        return _mapper.Map<IEnumerable<LeaveTypeDto>>(leaveTypes);
+    }
+}
