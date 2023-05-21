9cc13e2eb78949ba6dbc2e7dd4a045c0c3db5e4f
Armin.Yaghoubi <Armin.Yaghoubi@IT-Yaghoubi>
2023-05-21 15:36:21 +0330

    #8 Has been Created LeaveTypeDetails Feature

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetDetails/GetLeaveTypeDetailsQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistance;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetDetails;
+
+public class GetLeaveTypeDetailsQueryHandler : IRequestHandler<GetLeaveTypeDetailsQuery, LeaveTypeDetailsDto>
+{
+    private readonly IMapper _mapper;
+    private readonly IGenericRepository<Domain.LeaveType> _repository;
+
+    public GetLeaveTypeDetailsQueryHandler(IMapper mapper, IGenericRepository<Domain.LeaveType> repository)
+    {
+        _mapper = mapper;
+        _repository = repository;
+    }
+
+    public async Task<LeaveTypeDetailsDto> Handle(GetLeaveTypeDetailsQuery request, CancellationToken cancellationToken)
+    {
+        var leaveType = await _repository.GetByIdAsync(request.Id, cancellationToken);
+
+        return _mapper.Map<LeaveTypeDetailsDto>(leaveType);
+    }
+}
