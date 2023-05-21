4de4dd95e653cba03ad2a5db064d22e9bf73f190
Armin.Yaghoubi <Armin.Yaghoubi@IT-Yaghoubi>
2023-05-21 16:08:05 +0330

    Add LeaveTypeDetailsDto to Mapping

--- HR.LeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
+++ HR.LeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAll;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetDetails;
 using HR.LeaveManagement.Domain;
 
 namespace HR.LeaveManagement.Application.MappingProfiles;
@@ -9,5 +10,6 @@ public class LeaveTypeProfile : Profile
     public LeaveTypeProfile()
     {
         CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+        CreateMap<LeaveType, LeaveTypeDetailsDto>();
     }
 }
