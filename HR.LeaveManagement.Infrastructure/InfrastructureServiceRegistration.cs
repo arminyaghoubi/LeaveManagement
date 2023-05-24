4f34d1ac0e599be895341423e9c961552e581407
Armin.Yaghoubi <Armin.Yaghoubi@IT-Yaghoubi>
2023-05-24 19:20:40 +0330

    Add InfrastructureServiceRegistration

---
+++ HR.LeaveManagement.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Infrastructure;
+
+public static class InfrastructureServiceRegistration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services) => services;
+}
