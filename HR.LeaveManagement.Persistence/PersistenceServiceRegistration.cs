58c8d8e1047db8d9fc6b9328391b13b88aac622f
Armin.Yaghoubi <Armin.Yaghoubi@IT-Yaghoubi>
2023-05-28 15:36:10 +0330

    Change Something

--- HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
+++ HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -11,5 +11,5 @@ public static class PersistenceServiceRegistration
 {
     public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration) =>
         services.AddDbContext<HrDatabaseContext>(options => options.UseSqlServer(configuration.GetConnectionString("HrDatabaseContext")))
-        .AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<,>));
+        .AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 }
