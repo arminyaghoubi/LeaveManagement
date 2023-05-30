bc13c51ee85cc5b0a8a225bedfb8acfd1036698c
Armin.Yaghoubi <Armin.Yaghoubi@IT-Yaghoubi>
2023-05-30 17:24:33 +0330

    #22 Has been Created the Api Project and injected all services in other layers

---
+++ HR.LeaveManagement.Api/Program.cs
@@ -0,0 +1,49 @@
+using HR.LeaveManagement.Application;
+using HR.LeaveManagement.Infrastructure;
+using HR.LeaveManagement.Persistence;
+
+var myAllowSpecificOrigins = "All";
+
+var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: myAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                      });
+});
+
+// Add services to the container.
+builder.Services.AddApplicationServices()
+    .AddInfrastructureServices(configuration)
+    .AddPersistenceServices(configuration);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
